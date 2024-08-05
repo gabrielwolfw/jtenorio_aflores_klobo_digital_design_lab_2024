46a8252d81cc3e4c87c55ad1f4aa059fb8dcfc4b
Kevin Lobo <67599399+gabrielwolfw@users.noreply.github.com>
2024-08-04 19:31:45 -0600

    testbench añadido

--- Laboratorio_1/Problema_3_RestadorParametrizado/RestadorParametrizado_tb.sv
+++ Laboratorio_1/Problema_3_RestadorParametrizado/RestadorParametrizado_tb.sv
@@ -1,1 +1,21 @@
-//Aquí se realizará el auto chequeo de test bench
+module RestadorParametrizado_tb;
+    parameter N = 8;  // Parámetro del tamaño de los bits
+
+    logic [N-1:0] a, b;  // Entradas del restador
+    logic reset;         // Señal de reset asíncrono
+    logic clk;           // Señal de reloj
+    logic [N-1:0] y;     // Salida del restador
+
+    RestadorParametrizado #(N) uut (
+        .a(a),
+        .b(b),
+        .reset(reset),
+        .clk(clk),
+        .y(y)
+    );
+
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;  // Generador de reloj
+    end
+endmodule
