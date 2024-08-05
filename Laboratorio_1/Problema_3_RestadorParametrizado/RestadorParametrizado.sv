82a76a3b87b9d1085575c0ef9b1d80939e62fbf5
Kevin Lobo <67599399+gabrielwolfw@users.noreply.github.com>
2024-08-04 18:37:48 -0600

    se cambio la estructura de restador parametrizado

--- Laboratorio_1/Problema_3_RestadorParametrizado/RestadorParametrizado.sv
+++ Laboratorio_1/Problema_3_RestadorParametrizado/RestadorParametrizado.sv
@@ -1,16 +1,23 @@
 module RestadorParametrizado #(parameter int N = 8)
 (
-	input logic [N-1:0] a, b, 
-	input logic clk,
-	input logic asyn_reset,
-	output logic [N-1:0] z 
+    input logic [N-1:0] a,   // Entrada A
+    input logic [N-1:0] b,   // Entrada B
+    input logic reset,       // Señal de reset asíncrono
+    input logic clk,         // Señal de reloj
+    output logic [N-1:0] y   // Salida del resultado de la resta
 );
+	 // Registro para almacenar el resultado
+    logic [N-1:0] result;
 
-always_ff @(posedge clk or posedge async_reset) begin
-    if (async_reset) begin
-        z <= '0; //Se añade los ceros
-    end else begin
-        z <= a - b; //Se aplica la resta
+    always_ff @(posedge clk or negedge reset) begin
+        if (!reset) begin
+            result <= '0;
+        end else begin
+            result <= a - b;
+        end
     end
-end
+
+    // La salida del módulo es el resultado registrado
+    assign y = result;
+
 endmodule
