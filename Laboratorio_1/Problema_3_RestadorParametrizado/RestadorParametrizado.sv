23c6b51cc3afe6b86e6eb118e9b3414a7b8eb09f
Kevin Lobo <67599399+gabrielwolfw@users.noreply.github.com>
2024-08-07 10:33:41 -0600

    cambio en el restador parametrizado

--- Laboratorio_1/Problema_3_RestadorParametrizado/RestadorParametrizado.sv
+++ Laboratorio_1/Problema_3_RestadorParametrizado/RestadorParametrizado.sv
@@ -1,23 +1,14 @@
-module RestadorParametrizado #(parameter int N = 8)
-(
-    input logic [N-1:0] a,   // Entrada A
-    input logic [N-1:0] b,   // Entrada B
-    input logic reset,       // Señal de reset asíncrono
-    input logic clk,         // Señal de reloj
-    output logic [N-1:0] y   // Salida del resultado de la resta
+module parametrizable_subtractor #(parameter N = 6)(
+    input logic clk,
+    input logic reset,
+    input logic [N-1:0] initial_value,
+    input logic dec,
+    output logic [N-1:0] count
 );
-	 // Registro para almacenar el resultado
-    logic [N-1:0] result;
-
-    always_ff @(posedge clk or negedge reset) begin
-        if (!reset) begin
-            result <= '0;
-        end else begin
-            result <= a - b;
-        end
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset)
+            count <= initial_value;
+        else if (dec)
+            count <= count - 1;
     end
-
-    // La salida del módulo es el resultado registrado
-    assign y = result;
-
 endmodule
