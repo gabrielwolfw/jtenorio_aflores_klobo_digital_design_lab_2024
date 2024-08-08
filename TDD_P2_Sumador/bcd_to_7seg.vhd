3fe84782123bfc010aba2901a2b72110aefc9c49
JavsRecTc02 <tenoriojavierc@gmail.com>
2024-08-08 01:17:21 -0600

    Orden- 7-Segment
    Se modifico el orden de la conversion 1/0 de los 7 segmentos

--- TDD_P2_Sumador/bcd_to_7seg.vhd
+++ TDD_P2_Sumador/bcd_to_7seg.vhd
@@ -13,16 +13,16 @@ begin
     process(bcd)
     begin
         case bcd is
-            when "0000" => seg <= "0000001"; -- 0
-            when "0001" => seg <= "1001111"; -- 1
-            when "0010" => seg <= "0010010"; -- 2
-            when "0011" => seg <= "0000110"; -- 3
-            when "0100" => seg <= "1001100"; -- 4
-            when "0101" => seg <= "0100100"; -- 5
-            when "0110" => seg <= "0100000"; -- 6
-            when "0111" => seg <= "0001111"; -- 7
+            when "0000" => seg <= "1000000"; -- 0
+            when "0001" => seg <= "1111001"; -- 1
+            when "0010" => seg <= "0100100"; -- 2
+            when "0011" => seg <= "0110000"; -- 3
+            when "0100" => seg <= "0011001"; -- 4
+            when "0101" => seg <= "0010010"; -- 5
+            when "0110" => seg <= "0000010"; -- 6
+            when "0111" => seg <= "1111000"; -- 7
             when "1000" => seg <= "0000000"; -- 8
-            when "1001" => seg <= "0000100"; -- 9
+            when "1001" => seg <= "0010000"; -- 9
             when others => seg <= "1111111"; -- Display apagado para entradas inválidas
         end case;
     end process;
