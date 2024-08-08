6f33bb35e1ad2d3586d7fcab41f07c7790f09083
Axel Flores Lara <129690552+Axelsofresh@users.noreply.github.com>
2024-08-08 06:57:05 -0600

    Update DecoderToBcd.sv

--- TDD_P1_Decoder/DecoderToBcd.sv
+++ TDD_P1_Decoder/DecoderToBcd.sv
@@ -6,22 +6,26 @@ module DecoderToBcd(
   always @(*) begin
     case (in)
       4'b0000: out = 8'b0000_0000; // 00
-      4'b0001: out = 8'b0000_0001; // 01
-      4'b0010: out = 8'b0000_0010; // 02
-      4'b0011: out = 8'b0000_0011; // 03
-      4'b0100: out = 8'b0000_0100; // 04
-      4'b0101: out = 8'b0000_0101; // 05
-      4'b0110: out = 8'b0000_0110; // 06
-      4'b0111: out = 8'b0000_0111; // 07
-      4'b1000: out = 8'b0000_1000; // 08
-      4'b1001: out = 8'b0000_1001; // 09
-      4'b1010: out = 8'b0001_0000; // 10
-      4'b1011: out = 8'b0001_0001; // 11
-      4'b1100: out = 8'b0001_0010; // 12
-      4'b1101: out = 8'b0001_0011; // 13
-      4'b1110: out = 8'b0001_0100; // 14
-      4'b1111: out = 8'b0001_0101; // 15
+      4'b0001: out = 8'b0000_0001; // .
+      4'b0010: out = 8'b0000_0010; // .
+      4'b0011: out = 8'b0000_0011; // .
+      4'b0100: out = 8'b0000_0100; // .
+      4'b0101: out = 8'b0000_0101; // .
+      4'b0110: out = 8'b0000_0110; // .
+      4'b0111: out = 8'b0000_0111; // .
+      4'b1000: out = 8'b0000_1000; // .
+      4'b1001: out = 8'b0000_1001; // .
+      4'b1010: out = 8'b0001_0000; // .
+      4'b1011: out = 8'b0001_0001; // .
+      4'b1100: out = 8'b0001_0010; // .
+      4'b1101: out = 8'b0001_0011; // .
+      4'b1110: out = 8'b0001_0100; // .
+      4'b1111: out = 8'b0001_0101; // .
       default: out = 8'b0000_0000; // Apagado para entradas no válidas
     endcase
   end
 endmodule
+
+//El código emplea el modelo de comportamiento mediante el always, cambiando instantaneamente
+//cada salida según la entrada, se describe en alto nivel cómo se debe decodificar
+//sin específicar sus componentes físicos necesarios
