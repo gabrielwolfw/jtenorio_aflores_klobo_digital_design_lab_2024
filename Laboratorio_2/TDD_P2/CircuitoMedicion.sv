a084d34ed04ca62215c514699758c4c02d958073
Kevin Lobo <67599399+gabrielwolfw@users.noreply.github.com>
2024-08-23 04:00:42 -0600

    se añadió los registros y alu en paralelo

--- Laboratorio_2/TDD_P2/CircuitoMedicion.sv
+++ Laboratorio_2/TDD_P2/CircuitoMedicion.sv
@@ -4,8 +4,31 @@ module CircuitoMedicion #(parameter N = 8)(
 	input wire [N-1:0] in, 	//Entrada de datos
 	output wire [N-1:0] out //Salida de datos
 );
-	wire [N-1:0] reg1_out; //Se debe crear un modulo de registro
-	wire [N-1:] alu_out;   //Añadir la ALU
+	wire [N-1:0] reg1_out; //Se 
+	wire [N-1:] alu_out;   //Salida de ALU
 	
+	//Registro de entrada
+	Register #(N) reg1(
+		.clk(clk),
+		.reset(reset),
+		.D(in),
+		.Q(reg1_out)
+	);
+	
+	ALU #(N) alu1(
+		.a(reg1_out),
+		.b(in_b),
+		.operation(operation)
+	);
+	
+	
+	
+	// Registro de salida
+   Register #(N) reg2 (
+        .clk(clk),
+        .reset(reset),
+        .D(alu_out),
+        .Q(out)
+    );
 	
 endmodule
