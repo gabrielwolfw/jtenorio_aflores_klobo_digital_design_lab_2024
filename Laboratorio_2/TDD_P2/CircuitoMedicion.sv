c618e18101257754215b51ddb7028573e813642a
Kevin Lobo <67599399+gabrielwolfw@users.noreply.github.com>
2024-08-23 04:01:24 -0600

    cambios en los parametros de entrada y salida del alu

--- Laboratorio_2/TDD_P2/CircuitoMedicion.sv
+++ Laboratorio_2/TDD_P2/CircuitoMedicion.sv
@@ -1,34 +1,44 @@
-module CircuitoMedicion #(parameter N = 8)(
-	input wire clk,			//Señal de reloj	
-	input wire reset,			//Señal de reset
-	input wire [N-1:0] in, 	//Entrada de datos
-	output wire [N-1:0] out //Salida de datos
+module CircuitoMedicion #(parameter N = 4) (
+    input wire clk,
+    input wire reset,
+    input wire [N-1:0] in_a,
+    input wire [N-1:0] in_b,
+    input wire [3:0] operation,
+    output wire [N-1:0] out,
+    output wire outFlagC,
+    output wire outFlagN,
+    output wire outFlagV,
+    output wire outFlagZ,
+    output wire [6:0] segA
 );
-	wire [N-1:0] reg1_out; //Se 
-	wire [N-1:] alu_out;   //Salida de ALU
-	
-	//Registro de entrada
-	Register #(N) reg1(
-		.clk(clk),
-		.reset(reset),
-		.D(in),
-		.Q(reg1_out)
-	);
-	
-	ALU #(N) alu1(
-		.a(reg1_out),
-		.b(in_b),
-		.operation(operation)
-	);
-	
-	
-	
-	// Registro de salida
-   Register #(N) reg2 (
+    wire [N-1:0] reg1_out;
+    wire [N-1:0] alu_out;
+
+    // Primer registro
+    Register #(N) reg1 (
+        .clk(clk),
+        .reset(reset),
+        .D(in_a),
+        .Q(reg1_out)
+    );
+
+    // ALU
+    ALU #(N) alu1 (
+        .a(reg1_out),
+        .b(in_b),
+        .operation(operation),
+        .outFlagC(outFlagC),
+        .outFlagN(outFlagN),
+        .outFlagV(outFlagV),
+        .outFlagZ(outFlagZ),
+        .segA(segA)
+    );
+
+    // Segundo registro
+    Register #(N) reg2 (
         .clk(clk),
         .reset(reset),
         .D(alu_out),
         .Q(out)
     );
-	
 endmodule
