53573991777de8097104c23c4c4b0e09e9f0493d
JavsRecTc02 <tenoriojavierc@gmail.com>
2024-09-29 23:22:08 -0600

    Estados de la MEF actualizados

--- Laboratorio_3/VGA/toplevel_tic_tac_toe.sv
+++ Laboratorio_3/VGA/toplevel_tic_tac_toe.sv
@@ -1,23 +1,23 @@
-module toplevel_tic_tac_toe ( input logic I,  T, W,  A, rst, clk, 
+module toplevel_tic_tac_toe (
+    input logic I, T, W, A, PLAYER_1, PLAYER_2, rst, clk, 
     output logic [3:0] estado, // Asumimos 4 bits para los estados
     output logic vgaclk, hsync, vsync, sync_b, blank_b, // Señales VGA
     output logic [7:0] r, g, b, // Señales de color VGA
-	 output logic [8:0] matrix_out_MEF,  // Salida de la matriz modificada
-	 output logic load,         // Señal de carga de la matriz
-	 //output logic finished,    // Señal que indica que el contador ha llegado a 15 segundos
-    output logic [6:0] segments  // Segmentos del primer display de 7 segmentos (decenas)
-    //output logic [6:0] seg2   // Segmentos del segundo display de 7 segmentos (unidades)
+    output logic [8:0] matrix_out_MEF,  // Salida de la matriz modificada
+    output logic load,         // Señal de carga de la matriz
+    output logic [6:0] segments,  // Segmentos del primer display de 7 segmentos
+    output logic led_p1, led_p2  // LEDs para indicar turno de los jugadores
 );
 
-    //logic C;
     logic [9:0] x, y;
     logic [7:0] r_PantallaInicial, g_PantallaInicial, b_PantallaInicial; // Señales para Pantalla Inicial
     logic [7:0] r_videoGen, g_videoGen, b_videoGen; // Señales para videoGen
     logic [7:0] r_black, g_black, b_black; // Señales para pantalla en negro
-	 logic finished;   // Señal que indica que el contador ha llegado a 15 segundos
-	 logic [3:0] count;
-	
-	 
+    logic finished;   // Señal que indica que el contador ha llegado a 15 segundos
+    logic [3:0] count; // Temporizador de 4 bits
+    logic reset_timer; // Control del temporizador
+    logic reset_done;  // Señal de control del reset
+
     // Instancia de PLL para generar la señal de 25.175 MHz para el VGA
     pll vgapll(.inclk0(clk), .c0(vgaclk));
 
@@ -32,102 +32,122 @@ module toplevel_tic_tac_toe ( input logic I,  T, W,  A, rst, clk,
         .g(g_PantallaInicial), 
         .b(b_PantallaInicial)
     );
-	 
-	 // Instancia del Tiempo
-	 
-	  Full_Timer timer_inst (
+    
+    // Instancia del Temporizador
+    Full_Timer timer_inst (
         .clk_in(clk),
-        .rst_in(rst),
+        .rst_in(reset_timer),
         .done(finished),
         .count_out(count)
     );
-	 
-	 BCD_Visualizer segBCD_inst (
+    
+    BCD_Visualizer segBCD_inst (
         .bin(count),
         .seg(segments)
     );
-	 
-	 // Instancia del módulo videoGen
+    
+    // Instancia del módulo videoGen
     videoGen vgagen(
         .x_pos(x), 
         .y_pos(y), 
-		  .grid_state(matrix_out_MEF),
+        .grid_state(matrix_out_MEF),
         .red(r_videoGen), 
         .green(g_videoGen), 
         .blue(b_videoGen)
     );
-	 
-	 logic [8:0] matrix_in;
-	 logic [8:0] matrix_reg;    // Almacena la matriz de salida de matrixRegister
-	 
-	 initial begin
-        matrix_reg = 9'b10;  // Inicializar la matriz con ceros
+
+    logic [8:0] matrix_in;
+    logic [8:0] matrix_reg;    // Almacena la matriz de salida de matrixRegister
+    
+    initial begin
+        matrix_reg = 9'b10;  // Inicializar la matriz
     end
-	 //assign matrix_out_MEF = 9'b0;
-	 
-	 
-	 logic rst_n;
-	 assign rst_n = !rst;
-	 
-	 
-	 
-	 
+    
+    logic rst_n;
+    assign rst_n = !rst;
+
     // Instancia del módulo debounce_better_version
-    Button_debounce (
-        .button_in(I),       // Entrada del botón con rebote
-        .clk_in(clk),           // Reloj principal del sistema
-        .button_stable(Idebounced)   // Señal de salida debounced
+    Button_debounce debounce_I (
+        .button_in(I),
+        .clk_in(clk),
+        .button_stable(Idebounced)
     );
-	  Button_debounce (
-        .button_in(W),       // Entrada del botón con rebote
-        .clk_in(clk),           // Reloj principal del sistema
-        .button_stable(Wdebounced)   // Señal de salida debounced
+    
+    Button_debounce debounce_W (
+        .button_in(W),
+        .clk_in(clk),
+        .button_stable(Wdebounced)
     );
 
     // Instancia del módulo matrixControl
     matrixTableroControl u_matrixControl (
-        .clk(clk),              // Señal de reloj
-        .rst_n(rst_n),          // Señal de reset activo bajo
-        .I(I),                  // Botón para seleccionar la posición de la matriz
-        .W(!Wdebounced),                  // Botón para confirmar y modificar la posición seleccionada
-        .matrix_in(matrix_reg),   // Matriz de entrada (inicial)
-        .matrix_out(matrix_in), // Matriz de salida con el valor modificado
-        .load(load)             // Señal que indica que se realizó un cambio
+        .clk(clk),
+        .rst_n(rst_n),
+        .I(I),
+        .W(!Wdebounced),
+        .matrix_in(matrix_reg),
+        .matrix_out(matrix_in),
+        .load(load)
     );
-	 
-	 // Instancia del módulo matrixRegister
+
+    // Instancia del módulo matrixRegister
     matrixTablero u_matrixRegister (
-        .clk(clk),              // Señal de reloj
-        .rst_n(rst_n),          // Señal de reset activo bajo
-        .data_in(matrix_in),    // Datos modificados que se cargarán en el registro
-        .load(load),            // Señal de carga desde matrixControl
-        .matrix(matrix_reg)     // Matriz de salida del registro
+        .clk(clk),
+        .rst_n(rst_n),
+        .data_in(matrix_in),
+        .load(load),
+        .matrix(matrix_reg)
     );
-	 
-	 assign matrix_out_MEF = matrix_reg;
+    
+    assign matrix_out_MEF = matrix_reg;
 
     // Pantalla en negro (todos los píxeles son 0)
     assign r_black = 8'b00000000;
     assign g_black = 8'b00000000;
     assign b_black = 8'b00000000;
 
-    // Definir los estados de la MEF
+    // Definición de los estados
     typedef enum logic [3:0] {
-        P_INICIO = 4'b0000, TABLERO = 4'b0001
-		  //, S2 = 4'b0010, S3 = 4'b0011,
-//        S4 = 4'b0100, S5 = 4'b0101, S6 = 4'b0110, S7 = 4'b0111, 
-//        S8 = 4'b1000, S9 = 4'b1001
+        P_INICIO = 4'b0000,
+        TABLERO = 4'b0001,
+        TURNO_P1 = 4'b0010,   // Turno del jugador 1
+        TURNO_P2 = 4'b0011,   // Turno del jugador 2
+        CHECK_WIN = 4'b0100,  // Revisión del ganador
+        GAME_OVER = 4'b0101   // Juego terminado
     } state_t;
 
     state_t current_state, next_state;
 
-
-    // Actualización de estado
+    // Actualización de estado y control del temporizador
     always_ff @(posedge clk or posedge rst) begin
-        if (rst)
+        if (rst) begin
             current_state <= P_INICIO;
-        else
+            reset_timer <= 1'b1; // Resetear el temporizador al iniciar
+            reset_done <= 1'b0;  // Asegurarse de que no se ha completado el reset
+        end else begin
             current_state <= next_state;
+
+            case (next_state)
+                // Reseteamos el temporizador en TABLERO
+                TABLERO: begin
+                    reset_timer <= 1'b1;
+                    reset_done <= 1'b0;
+                end
+                // En TURNO_P1 y TURNO_P2, el temporizador cuenta hasta F y cambia de turno
+                TURNO_P1, TURNO_P2: begin
+                    if (!reset_done) begin
+                        reset_timer <= 1'b1;  // Reseteamos el temporizador al cambiar de turno
+                        reset_done <= 1'b1;
+                    end else begin
+                        reset_timer <= 1'b0;  // Permitimos que el temporizador cuente
+                    end
+                end
+                default: begin
+                    reset_timer <= 1'b0;
+                    reset_done <= 1'b0;
+                end
+            endcase
+        end
     end
 
     // Lógica de transición de estados
@@ -135,25 +155,39 @@ module toplevel_tic_tac_toe ( input logic I,  T, W,  A, rst, clk,
         next_state = current_state;
         case (current_state)
             P_INICIO: begin
-                if (T) 
-                    next_state = TABLERO; 
-//                else if (A)
-//                    next_state = S5;
-					 else next_state= P_INICIO;
+                if (T)
+                    next_state = TABLERO;
+                else
+                    next_state = P_INICIO;
             end
             TABLERO: begin
-//                if (G)
-//                    next_state = S3;
-//                else if (P)
-//                    next_state = S4;
-                 if (A)
-							next_state=P_INICIO;
-                    //next_state = S2;
-//                else if (L)
-//                    next_state = S0;
-					 else next_state= TABLERO;
+                if (PLAYER_1)
+                    next_state = TURNO_P1;
+                else if (PLAYER_2)
+                    next_state = TURNO_P2;
             end
-
+            TURNO_P1: begin
+                if (finished || count == 4'hF)  // Cambiar de turno si el temporizador llega a F
+                    next_state = TURNO_P2;
+                else if (A)
+                    next_state = CHECK_WIN;
+            end
+            TURNO_P2: begin
+                if (finished || count == 4'hF)  // Cambiar de turno si el temporizador llega a F
+                    next_state = TURNO_P1;
+                else if (A)
+                    next_state = CHECK_WIN;
+            end
+            CHECK_WIN: begin
+                if (A)
+                    next_state = GAME_OVER;
+                else
+                    next_state = TABLERO;
+            end
+            GAME_OVER: begin
+                if (A)
+                    next_state = P_INICIO;
+            end
             default: next_state = P_INICIO;
         endcase
     end
@@ -165,23 +199,44 @@ module toplevel_tic_tac_toe ( input logic I,  T, W,  A, rst, clk,
         else
             estado <= current_state;
     end
+    
+    // Control de los LEDs de los jugadores
+	 always_ff @(posedge clk or posedge rst) begin
+		 if (rst) begin
+			 led_p1 <= 1'b1;  // Inicializamos los LEDs encendidos
+			 led_p2 <= 1'b1;
+		 end else begin
+			 case (current_state)
+					 TURNO_P1: begin
+						 led_p1 <= 1'b1;   // Encendemos el LED del Jugador 1
+						 led_p2 <= 1'b0;   // Apagamos el LED del Jugador 2
+					 end
+					 TURNO_P2: begin
+						 led_p1 <= 1'b0;   // Apagamos el LED del Jugador 1
+						 led_p2 <= 1'b1;   // Encendemos el LED del Jugador 2
+					 end
+					 default: begin
+						 led_p1 <= 1'b0;   // Apagamos ambos LEDs en cualquier otro estado
+						 led_p2 <= 1'b0;
+					 end
+			 endcase
+		 end
+	 end
+
 
     // Multiplexor para seleccionar qué imagen mostrar
     always_comb begin
         case (current_state)
-            // En el estado P_INICIO mostramos la imagen generada por Pantalla_Inicial
             P_INICIO: begin
                 r = r_PantallaInicial;
                 g = g_PantallaInicial;
                 b = b_PantallaInicial;
             end
-            // En el estado TABLERO mostramos la imagen generada por videoGen
-            TABLERO: begin
+            TABLERO, TURNO_P1, TURNO_P2, CHECK_WIN, GAME_OVER: begin
                 r = r_videoGen;
                 g = g_videoGen;
                 b = b_videoGen;
             end
-            // Al cambiar de estado, poner la pantalla en negro para "borrar"
             default: begin
                 r = r_black;
                 g = g_black;
